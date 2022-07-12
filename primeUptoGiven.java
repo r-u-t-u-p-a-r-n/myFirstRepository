50d46f3edf74f27f721b29f19d6bdbc47eaa8827
Rutuparn <durgawadr@gmail.com>
2022-07-13 02:37:28 +0530

    Question 13

---
+++ primeUptoGiven.java
@@ -0,0 +1,30 @@
+// 13. Programm to count all prime numbers upto given number.
+
+import java.util.Scanner;
+public class primeUptoGiven
+{
+    public static void main(String args[])
+    {
+      Scanner s = new Scanner(System.in);
+      System.out.print("Enter a number (must be an integer) : ");
+      int ll = s.nextInt();
+      int i = 1 ;
+      System.out.print("Prime numbers upto given number is : ");
+      while(i<=ll)
+      {
+        if(checkForPrime(i)) { System.out.print(i+" "); }
+        i++ ;
+      }
+    }
+
+    public static boolean checkForPrime(int a)
+    {
+    	int i = 1, num = i-1 ;
+    	while(i<=a)
+    	{
+    		if(a%(i++) == 0) { num++ ; }
+    		if(num > 2) { return false ; }
+    	}
+    	return true ;
+    }
+}
